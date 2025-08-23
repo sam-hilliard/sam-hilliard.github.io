f844a3d4a1af10c08cf256db2028e04dd36de7e0
Sam Hilliard <samhilliard51@gmail.com>
2025-08-23 12:13:36 -0400

    Reset boiler plate

--- client/src/components/LoadingAnimation/LoadingAnimation.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function LoadingAnimation() {
-  return (
-    <div className="container main-container centered"><div className="lds-dual-ring"></div></div>
-  )
-}
